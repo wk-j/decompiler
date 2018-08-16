796e8cf64d658317fc7e6db402d9fca632973a7d
wk-j <somnuk.wk@gmail.com>
2018-08-17 02:09:15 +0700

    Decompile script

--- scripts/Hello/H.csx
+++ scripts/Hello/H.csx
@@ -1,7 +1,2 @@
-using System;
 
-class Program {
-    public static void Main(string[] args) {
-        Console.WriteLine("Hello, world!");
-    }
-}
+Console.WriteLine("Hello, world!");
--- src/Decompiler/Processor.cs
+++ src/Decompiler/Processor.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using ICSharpCode.Decompiler;
 using ICSharpCode.Decompiler.CSharp;
@@ -11,7 +13,9 @@ using ICSharpCode.Decompiler.Metadata;
 using ICSharpCode.Decompiler.TypeSystem;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
 using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CodeAnalysis.Scripting;
 
 class Processor {
     private Microsoft.CodeAnalysis.SyntaxTree Parse(string code) {
@@ -27,25 +31,72 @@ class Processor {
         return refs;
     }
 
-    private CSharpCompilation CompileTree(Microsoft.CodeAnalysis.SyntaxTree tree, OptimizationLevel level) {
+    private ImmutableArray<string> GetUsings() {
+        var usings = ImmutableArray.Create(
+            "System",
+            "System.IO",
+            "System.Collections.Generic",
+            "System.Console",
+            "System.Diagnostics",
+            // "System.Dynamic",
+            // "System.Linq",
+            // "System.Linq.Expressions",
+            "System.Text",
+            "System.Threading.Tasks"
+        );
+        return usings;
+    }
+
+
+    private Compilation CompileCSharp(Microsoft.CodeAnalysis.SyntaxTree tree, OptimizationLevel level) {
+        var references = new MetadataReference[] {
+            MetadataReference.CreateFromFile(typeof(Object).Assembly.Location),
+            MetadataReference.CreateFromFile(typeof(Console).Assembly.Location),
+        };
+        var all = references.Concat(GetReferences());
+        var usings = GetUsings();
+        var com = CSharpCompilation.Create(
+            "Hello",
+            syntaxTrees: new[] { tree },
+            references: all,
+            options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+                .WithScriptClassName("Hello")
+                .WithOptimizationLevel(level)
+                .WithUsings(usings)
+        );
+        return com;
+    }
+
+    private Compilation CompileScript(Microsoft.CodeAnalysis.SyntaxTree tree, OptimizationLevel level) {
         var references = new MetadataReference[] {
             MetadataReference.CreateFromFile(typeof(Object).Assembly.Location),
             MetadataReference.CreateFromFile(typeof(Console).Assembly.Location),
         };
 
         var all = references.Concat(GetReferences());
+        var usings = GetUsings();
+        var options = ScriptOptions.Default
+            .WithImports(usings)
+            //.WithEmitDebugInformation(level == OptimizationLevel.Debug ? true : false)
+            .WithReferences(references);
 
-        var com = CSharpCompilation.Create(
-            "Hello",
-            syntaxTrees: new[] { tree },
-            references: all,
-            options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
-                .WithOptimizationLevel(level)
-        );
+        var script = CSharpScript.Create(tree.ToString(), options);
+        var com = script.GetCompilation();
+
+        if (level == OptimizationLevel.Release)
+            SetReleaseOptimizationLevel(com);
+
         return com;
     }
 
-    private (bool, string) CreateDll(CSharpCompilation compile, String filePath) {
+    private static void SetReleaseOptimizationLevel(Compilation compilation) {
+        var compilationOptionsField = typeof(CSharpCompilation).GetTypeInfo().GetDeclaredField("_options");
+        var compilationOptions = (CSharpCompilationOptions)compilationOptionsField.GetValue(compilation);
+        compilationOptions = compilationOptions.WithOptimizationLevel(OptimizationLevel.Release);
+        compilationOptionsField.SetValue(compilation, compilationOptions);
+    }
+
+    private (bool, string) CreateDll(Compilation compile, String filePath) {
         using (var memory = new MemoryStream()) {
             var result = compile.Emit(memory);
             if (!result.Success) {
@@ -64,16 +115,16 @@ class Processor {
     private bool CreateDllFromCs(string csFile, string outputPath, OptimizationLevel level) {
         var code = File.ReadAllText(csFile);
         var tree = Parse(code);
-        var compile = CompileTree(tree, level);
+        var compile = CompileScript(tree, level);
         var (ok, _) = CreateDll(compile, outputPath);
         return ok;
     }
 
-    private void DecopmileDllToCs(string dllFile) {
+    private void DecopmileDllToCs(string dllFile, string csPath) {
         var dec = new CSharpDecompiler(dllFile, new DecompilerSettings());
-        var name = new FullTypeName("Program");
+        var name = new FullTypeName("Submission#0");
         var rs = dec.DecompileTypeAsString(name);
-        Console.WriteLine(rs);
+        File.WriteAllText(csPath, rs);
     }
 
     private void DecompileDllToIl(string dllFile, string ilPath) {
@@ -93,16 +144,20 @@ class Processor {
             OptimizationLevel.Release
         };
 
+        var dllPath = Path.ChangeExtension(csPath, "dll");
+
         foreach (var item in levels) {
-            var dllPath = Path.ChangeExtension(csPath, "dll");
             var ilPath = Path.ChangeExtension(csPath, $".{item}.il");
+            var newCsPath = Path.ChangeExtension(csPath, $"{item}.cs");
             var text = File.CreateText(ilPath);
             var ok = CreateDllFromCs(csPath, dllPath, item);
             if (ok) {
                 DecompileDllToIl(dllPath, ilPath);
-                File.Delete(dllPath);
             }
         }
+        // DecopmileDllToCs(dllPath, Path.ChangeExtension(csPath, $".Source.cake"));
+        File.Delete(dllPath);
+
         return "";
     }
 }
